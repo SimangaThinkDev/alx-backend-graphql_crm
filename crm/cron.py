8fa3f427d381f4644dae1848270a390d28135213
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-08-26 17:53:25 +0200

    _

--- crm/cron.py
+++ crm/cron.py
@@ -1,5 +1,11 @@
 import logging
 from datetime import datetime
+from gql.transport.requests import RequestsHTTPTransport
+from gql import (
+    gql,
+    Client,
+    GraphQLRequest
+)
 
 def log_crm_heartbeat():
     logging.basicConfig( filename="/tmp/crm_heartbeat_log.txt" )
@@ -7,3 +13,26 @@ def log_crm_heartbeat():
     # DD/MM/YYYY-HH:MM:SS CRM is alive
     logging.info( f"{datetime.now().isoformat()} CRM is alive" )
 
+def sample_test_query( endpoint:str, request_query:GraphQLRequest ):
+    transport = RequestsHTTPTransport(
+        url=endpoint,
+        verify=True,
+        retries=3,
+    )
+
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+
+    result = client.execute(request_query)
+
+if __name__ == "__main__":
+    
+    endpoint = "http://localhost:8000/graphql"
+    request_query = gql(
+        """
+        query {
+            hello
+        }
+        """
+    )
+
+    sample_test_query( endpoint=endpoint, request_query=request_query )
